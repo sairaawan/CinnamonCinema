fc82f0a8dd79ec594b217e859a6eb12294073ede
sairaawan <syraawan@live.com>
2022-02-14 09:36:59 +0000

    Created new method for new list

--- src/Reservation.java
+++ src/Reservation.java
@@ -2,13 +2,13 @@
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Random;
+
 
 public class Reservation {
     private String name;
     private final static List<String> totalSeats = new ArrayList<>(Arrays.asList("A1", "A2", "A3", "A4", "A5", "B1", "B2", "B3", "B4", "B5", "C1", "C2", "C3", "C4", "C5"));
-    private final int maxSeatsAllowed = 3;
-    private final int minSeatsAllowed = 1;
+    private final int maxSeatsAllowed=3;
+    private final int minSeatsAllowed=1;
     private int seatsRequested;
 
     public Reservation(String name, int seatsRequested) throws Exception {
@@ -32,24 +32,23 @@ public class Reservation {
             throw new Exception("You can reserve only 1-3 seats");
     }
 
-    public void reserveSeat(int seatsRequested) {
+    public String reserveSeat(int seatsRequested) {
+        System.out.println("Total size " + totalSeats.size());
+        String allocated = null;
         for (int i = 0; i < seatsRequested; i++) {
-            String allocated = totalSeats.get(i);
+           allocated=totalSeats.get(i);
             int ticketNo = i + 1;
             System.out.println(this.name + " " + ticketNo + " seat number is " + allocated);
         }
+        getTotalSeats();
+        return allocated;
+    }
 
+    public List<String> getTotalSeats() {
         totalSeats.subList(0, seatsRequested).clear();
         System.out.println("total seats left" + totalSeats);
-
+        return totalSeats;
     }
 
-    public static void main(String args[]) throws Exception {
-        Random random = new Random();
-        for (int i = 0; i < random.nextInt(1, 15); i++) {
-            Reservation reserve = new Reservation("Saira", random.nextInt(1, 3));
-        }
 
-
-    }
 }
